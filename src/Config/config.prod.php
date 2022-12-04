12c5fc03a4e63817ceefc7243c4ec4a747d2508f
Ilya Smirnov <ilua-smirnof@yandex.ru>
2022-12-04 23:02:45 +0100

    Update config.prod.php

--- src/Config/config.prod.php
+++ src/Config/config.prod.php
@@ -1,15 +1,9 @@
 <?php
 
-/* Fichier : /config/config.local.php */
+define('APP_DB_NAME', '');
 
-// le nom de la base
-define('APP_DB_NAME', 'flouet_avatar');
+define('APP_DB_HOST', '');
 
-// le nom de la machine qui héberge le serveur MYSQL
-define('APP_DB_HOST', 'mysql-flouet.alwaysdata.net');
+define('APP_DB_USER', '');
 
-// le nom de l'utilisateur MYSQL
-define('APP_DB_USER', 'flouet');
-
-// le mot de passe de l'utilisateur MYSQL
-define('APP_DB_PASSWORD', '4%h9xNOWNRV!t&1@sD$');
+define('APP_DB_PASSWORD', '');
