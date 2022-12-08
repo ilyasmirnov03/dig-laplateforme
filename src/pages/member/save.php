3aef7e3dc1044155187ace0501776ff10ac23ac3
Ilya Smirnov <ilua-smirnof@yandex.ru>
2022-12-08 22:40:01 +0100

    Refactoring calculator

--- src/pages/member/save.php
+++ src/pages/member/save.php
@@ -3,65 +3,112 @@
 use App\Helper\HTTP;
 use App\Model\Products;
 
+use function PHPSTORM_META\map;
+
 // calcul
 
-if ((int)$_POST['recup'] === 1){
+$categorieMatcher = [
+    1 => ['alimentation', 'cosmetique', 'textile'],
+    2 => ['electromenager', 'bricolage', 'mobilier', 'vehicule', 'entretien', 'loisirs'],
+    3 => ['maroquinerie']
+];
+
+$calculator = [
+    1 => [
+        'animal' => [
+            'vegan' => 1.5,
+            'vegetarien' => 1,
+            'autre' => 0
+        ],
+        'provenance' => [
+            'charente' => 5.5,
+            'nouvelle-aquitaine' => 4.5,
+            'france' => 3.5,
+            'europe' => 2,
+            'autre' => 0
+        ]
+    ],
+    2 => [
+        'provenance' => [
+            'charente' => 9,
+            'nouvelle-aquitaine' => 7,
+            'france' => 5,
+            'europe' => 2,
+            'autre' => 0
+        ]
+    ],
+    3 => [
+        'animal' => [
+            'vegan' => 3,
+            'vegetarien' => 0,
+            'autre' => 0
+        ],
+        'provenance' => [
+            'charente' => 6,
+            'nouvelle-aquitaine' => 5,
+            'france' => 3,
+            'europe' => 1,
+            'autre' => 0
+        ]
+    ]
+];
+
+if ((int)$_POST['recup'] === 1) {
     $leafScore = 10;
-}
-else{
+} else {
     $leafScore = 1;
-    switch ($_POST['categorie']){
-        case 'alimentation' || 'cosmetique' || 'textile' :
-            if ((int)$_POST['bio'] === 1){
+    switch ($_POST['categorie']) {
+        case 'alimentation' || 'cosmetique' || 'textile':
+            if ((int)$_POST['bio'] === 1) {
                 $leafScore += 2;
             }
-            if ($_POST['animal'] === 'vegan'){
+            if ($_POST['animal'] === 'vegan') {
                 $leafScore += 1.5;
             }
-            if ($_POST['animal'] === 'vegetarien'){
+            if ($_POST['animal'] === 'vegetarien') {
                 $leafScore += 1;
             }
-            if ($_POST['provenance'] === 'charente'){
+            if ($_POST['provenance'] === 'charente') {
                 $leafScore += 5.5;
             }
-            if ($_POST['provenance'] === 'nouvelle-aquitaine'){
+            if ($_POST['provenance'] === 'nouvelle-aquitaine') {
                 $leafScore += 4.5;
             }
-            if ($_POST['provenance'] === 'france'){
+            if ($_POST['provenance'] === 'france') {
                 $leafScore += 3.5;
             }
-            if ($_POST['provenance'] === 'europe'){
+            if ($_POST['provenance'] === 'europe') {
                 $leafScore += 2;
             }
             break;
         case 'electromenager' || 'bricolage' || 'mobilier' || 'vehicule' || 'entretien' || 'loisirs':
-            if ($_POST['provenance'] === 'charente'){
+            if ($_POST['provenance'] === 'charente') {
                 $leafScore += 9;
             }
-            if ($_POST['provenance'] === 'nouvelle-aquitaine'){
+            if ($_POST['provenance'] === 'nouvelle-aquitaine') {
                 $leafScore += 7;
             }
-            if ($_POST['provenance'] === 'france'){
+            if ($_POST['provenance'] === 'france') {
                 $leafScore += 5;
             }
-            if ($_POST['provenance'] === 'europe'){
+            if ($_POST['provenance'] === 'europe') {
                 $leafScore += 2;
             }
             break;
         case 'maroquinerie':
-            if ($_POST['animal'] === 'vegan'){
+            if ($_POST['animal'] === 'vegan') {
                 $leafScore += 3;
             }
-            if ($_POST['provenance'] === 'charente'){
+            if ($_POST['provenance'] === 'charente') {
                 $leafScore += 6;
             }
-            if ($_POST['provenance'] === 'nouvelle-aquitaine'){
+            if ($_POST['provenance'] === 'nouvelle-aquitaine') {
                 $leafScore += 5;
             }
-            if ($_POST['provenance'] === 'france'){
+            if ($_POST['provenance'] === 'france') {
                 $leafScore += 3;
             }
-            if ($_POST['provenance'] === 'europe'){
+            if ($_POST['provenance'] === 'europe') {
                 $leafScore += 1;
             }
             break;
